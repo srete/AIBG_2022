f03992f580c0d97a63f491c45a2ca8999dd051b8
Teodora Srećković <teodorasrec@gmail.com>
2022-12-09 23:47:58 +0100

    Parsing data from json example

---
+++ game_state_example.py
@@ -0,0 +1,17 @@
+import json
+
+with open('game_state.json', 'r') as f:
+    game_state = json.load(f)
+
+print('Game state:', game_state.keys())
+print('Game state data:', game_state['gameState'].keys())
+print('Player data:', game_state['gameState']['player1'].keys())
+print('Map data:', game_state['gameState']['map'].keys())
+
+# game_state['gameState']['map']['tiles'] je lista, gde je svaki element nova lista sa podacima o poljima koja imaju isto r 
+print('One tile data:', game_state['gameState']['map']['tiles'][0][0])
+
+print('Boss data: ', game_state['gameState']['boss'].keys())
+print('Boss action: ', game_state['gameState']['boss']['bossAction'], ', boss AttackedTiles: ', game_state['gameState']['boss']['bossAttackedTiles'])
+
+print('Player scoreBoard data:', game_state['gameState']['scoreBoard']['players'][0].keys())
