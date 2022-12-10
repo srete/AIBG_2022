84b9ad00bc0248f2e641c3229a5221e648e4c948
strajdzsha <superstrale2002@gmail.com>
2022-12-10 02:41:17 +0100

    ...

--- player.py
+++ player.py
@@ -1,9 +1,16 @@
 import json
+from map import Map
 
 class Player:
 
-    def __init__(self, _data) -> None:
+    def __init__(self, _data, _map:Map) -> None:
         self.data = _data
+        self.map = _map
+
+    def update_player(self, new_data, new_map):
+        ''' Updates player atributes'''
+        self.data = new_data
+        self.map = new_map
 
     def get_position(self):
         return(self.data["r"], self.data["q"])
@@ -26,7 +33,33 @@ class Player:
     def get_trap_duration(self):
         return(self.data["trapDuration"])
 
-    def turn(self):
-        # TODO
+    def move_to_center(self):
+        r, q = self.get_position()
+        next_r, next_q = 0, 0
+        if r > 0:
+            next_r = r - 1
+        elif r < 0:
+            next_r = r + 1
+        else:
+            next_r = r
 
-        pass
+        if q > 0:
+            next_q = q - 1
+        elif q < 0:
+            next_q = q + 1
+        else:
+            next_q = q
+
+        return next_r, next_q
+
+    def turn(self):
+        r, q = self.move_to_center()
+        turn = {"action":"move," + str(r) + "," + str(q)}
+
+        return turn
+
+"""
+    turn = {
+        "action":"move,r,q"
+    }
+"""
