1567a6e33ea26630f98739838832c3247bae0816
Teodora Srećković <teodorasrec@gmail.com>
2022-12-10 06:12:26 +0100

    Dodato odredjivanje zone

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import json
 from map import Map
+import pickle
 
 class Player:
 
@@ -44,6 +45,41 @@ class Player:
         turn = {"action":"move," + str(r) + "," + str(q)}
 
         return turn
+    
+    def tiles_distance(self, a: dict, b: dict) -> float:
+        return (abs(a['q'] - b['q']) 
+            + abs(a['q'] + a['r'] - b['q'] - b['r'])
+            + abs(a['r'] - b['r'])) / 2
+            
+    def get_zone(self) -> int:
+        '''
+        Prva: 2-4, Druga 5-10, Treca 11-14
+        '''  
+        # {'r': 0, 'q': 0}
+        r, q = self.get_position()
+        dist = self.tiles_distance({'r': 0, 'q': 0}, {'r': r, 'q': q})
+        if dist >= 2 and dist <= 4:
+            return 1
+        if dist >= 5 and dist <= 10:
+            return 2
+        if dist >= 11 and dist <= 14:
+            return 3
+        else:
+            return None
+        
+
+
+if __name__ == "__main__":
+    with open('generated_game_state.pkl', 'rb') as f:
+        game_state = pickle.load(f)
+        game_state = json.loads(game_state['gameState'])
+        game_state = {'gameState': game_state}
+
+        data = game_state['gameState']['player1']
+        map = game_state['gameState']['map']
+        player = Player(data, map)
+        print(player.get_zone())
+        print(player.get_position())
 
 """
     turn = {
